7558e55cb2d5c66f6b02953f524d13e83c3c9cc4
skygarden-lunard <159393970+skygarden-lunard@users.noreply.github.com>
2024-02-28 15:19:47 -0500

    figuring this out
    testolio

---
+++ Argos/TESTING/testolio.gd
@@ -0,0 +1,11 @@
+extends Node
+#testing to see if this uploads to github
+#update numero dos
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
